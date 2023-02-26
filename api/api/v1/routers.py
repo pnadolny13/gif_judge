48404a717ea25c74cc06e61eecb3d1cb418fe5a3
pnadolny13 <patnadolny@gmail.com>
2023-02-26 18:26:53 -0500

    add websocket for polling db

--- api/api/v1/routers.py
+++ api/api/v1/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .endpoints import gifs, games, players
+from .endpoints import gifs, games, players, websocket
 
 router = APIRouter()
 router.include_router(gifs.router)
 router.include_router(games.router)
 router.include_router(players.router)
+router.include_router(websocket.router)
